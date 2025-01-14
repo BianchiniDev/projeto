bee919f8de124787464ca32fdf0a63e84168be3f
JoaoDev <joaovitorbianchinidospassos@gmail.com>
2025-01-14 17:31:22 -0300

    mudançaaaaaa

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ function toggleMode() {
   if (html.classList.contains('light')) {
 
     img.setAttribute("src", "./assets/avatar-light.png")
-  
+  } else {
+
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
